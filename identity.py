5175654dba959edcd5e623b1ee5d9239d23d006e
songhanbyeol <127911993+songhanbyeol@users.noreply.github.com>
2023-10-23 10:19:18 +0900

    identity

---
+++ identity.py
@@ -0,0 +1,11 @@
+a = "ABE"
+b = a
+print( a is b)
+
+c = 2
+
+if c !=1:
+    print("1이아님")        #1이 아닐 경우 작동
+    
+if c ==1:
+    print("1임")            #1일경우 작동
---
+++ import math.py
@@ -0,0 +1,14 @@
+import math
+
+b = int(2.5)
+print(b)
+
+print(7)
+print("hello world")
+
+a = 1
+print(a)
+a *= 10
+print(a)
+a **= 10
+print(a)
